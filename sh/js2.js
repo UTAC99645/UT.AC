5fb22feaa8295ad6c2b7a16eccd72689135565a7
UTAC99645 <utac99645@gmail.com>
2024-10-13 16:32:19 +0800

    ud

---
+++ sh/js2.js
@@ -0,0 +1,12 @@
+let stars = document.getElementById('stars')
+let mountains_behind = document.getElementById('mountains_behind')
+let text = document.getElementById('text')
+let mountains_front = document.getElementById('mountains_front')
+let nano = document.getElementById('nano')
+window.addEventListener('scroll', function () {
+    let value = window.scrollY;
+    mountains_behind.style.top = value * 0.5 + 'ch'
+    mountains_front.style.top = value * 2 + 'px'
+    text.style.marginRight = value * 2 + 'px'
+    text.style.marginTop = value * 1.5 + 'px'
+})
