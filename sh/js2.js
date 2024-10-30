8d9feb945d05b27703f480b27b7d8345cc82fd90
UTAC <174389462+UTAC99645@users.noreply.github.com>
2024-10-30 12:40:19 +0800

    Update js2.js

--- sh/js2.js
+++ sh/js2.js
@@ -5,7 +5,7 @@ let mountains_front = document.getElementById('mountains_front')
 let nano = document.getElementById('nano')
 window.addEventListener('scroll', function () {
     let value = window.scrollY;
-    mountains_behind.style.top = value * 0.5 + 'ch'
+    mountains_behind.style.top = value * 4 + 'px'
     mountains_front.style.top = value * 2 + 'px'
     text.style.marginRight = value * 2 + 'px'
     text.style.marginTop = value * 1.5 + 'px'
