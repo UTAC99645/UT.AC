ec4ca48c3eee94baafa7e8f5ce944e3ebdea0b35
root <root@localhost>
2025-01-05 17:13:38 +0800

    ud

--- sh/js2.js
+++ sh/js2.js
@@ -1,12 +1,10 @@
 let stars = document.getElementById('stars')
-let mountains_behind = document.getElementById('mountains_behind')
+let ko = document.getElementById('ko')
 let text = document.getElementById('text')
-let mountains_front = document.getElementById('mountains_front')
 let nano = document.getElementById('nano')
 window.addEventListener('scroll', function () {
     let value = window.scrollY;
-    mountains_behind.style.top = value * 4 + 'px'
-    mountains_front.style.top = value * 2 + 'px'
+    ko.style.top = value * 4 + 'px'
     text.style.marginRight = value * 1.5 + 'px'
-    text.style.marginTop = value * 1.5 + 'px'
+    text.style.marginTop = value * 1.55 + 'px'
 })
